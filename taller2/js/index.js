c33b210c5c9cfb31e00e83a47006b61bc8ecc403
MateoZapata7 <mtzapatamontoya@gmail.com>
2023-05-29 18:32:23 -0500

    Se agrega el proyecto

---
+++ taller2/js/index.js
@@ -0,0 +1,31 @@
+const prueba = [{
+
+    title: "Mariscos",
+    dire: "Cll 07 # 18-94",
+    descr:"Los Mejores platos de mariscos de Cali",
+
+},
+{
+    title: "Paisas",
+    dire: "Cra 16 #3-13",
+    descr:"Deliciosas bandejas paisas",
+},
+{   
+    title: "Sancochox",
+    dire: "Cra 79 #29-34",
+    descr:"Los Mejores Sancochos de itagui",
+
+},
+{
+    title: "Coreans",
+    dire: "Avenida oriental Local 1",
+    descr:"La mejor comida coreana"
+}];
+
+export{prueba};
+
+
+
+
+
+
